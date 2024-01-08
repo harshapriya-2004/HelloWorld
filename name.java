658a1f85571c1a0cf737481ed8739beb24cff19e
harshapriya-2004 <harshapriyathiruveedhi1973@gmail.com>
2024-01-08 18:17:14 +0530

    added name program

---
+++ name.java
@@ -0,0 +1,8 @@
+public class name {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is harsha priya");
+        System.out.println("21MIC0125");
+
+    }
+}
